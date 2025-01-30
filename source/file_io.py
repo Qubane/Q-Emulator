97a186b042488e52d87f0347bbc7d417f5c29bc0
UltraQbik <no1skill@yandex.ru>
2025-01-30 13:59:57 +0300

    Define load function

--- source/file_io.py
+++ source/file_io.py
@@ -1,3 +1,11 @@
 """
 File reading and writing implementations.
 """
+
+
+def load(file: str):
+    """
+    Reads binary executable file, compiled by Q-Compiler
+    :param file: executable filepath
+    :return: list of instructions
+    """
