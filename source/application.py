1a24d9abb5123a3614e61f49ae570ca9f8dba794
UltraQbik <no1skill@yandex.ru>
2025-01-30 13:57:04 +0300

    Add simple argparser

--- source/application.py
+++ source/application.py
@@ -3,15 +3,37 @@ The main application class that is executed
 """
 
 
+from argparse import ArgumentParser, Namespace
+
+
 class Application:
     """
     Main application class
     """
 
     def __init__(self):
-        pass
+        self.args: Namespace | None = None
+
+    def parse_args(self):
+        """
+        Parses command line arguments
+        """
+
+        parser = ArgumentParser(prog="Q-Emulator", description="Emulates Quantum CPU's")
+        parser.add_argument("-i", "--input",
+                            help="binary file",
+                            required=True)
+        parser.add_argument("--namespace",
+                            help="code namespace",
+                            choices=["QT", "QM"],
+                            default="QT")
+
+        self.args = parser.parse_args()
 
     def run(self):
         """
         Runs the application
         """
+
+        # parse args
+        self.parse_args()
