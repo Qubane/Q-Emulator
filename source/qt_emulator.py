d345d51eabfb45c411f1e640cddea23914b53446
UltraQbik <no1skill@yandex.ru>
2025-01-31 15:55:57 +0300

    Get rid of annoying ctype integer classes For some reason you cannot use ctype integers as normal integers

--- source/qt_emulator.py
+++ source/qt_emulator.py
@@ -3,7 +3,7 @@ QT Architecture emulator
 """
 
 
-from ctypes import c_uint8, c_uint16, c_uint32
+from numpy import uint8, uint16, uint32, zeros, ndarray
 
 
 class QTEmulator:
@@ -15,28 +15,28 @@ class QTEmulator:
 
     def __init__(self):
         # program memory
-        self.rom: list[c_uint32] = list()
+        self.rom: list[uint32] = list()
 
         # array memory (cache)
-        self.cache: list[c_uint16] = list()
-        self.stack: list[c_uint16] = list()
-        self.address_stack: list[c_uint16] = list()
+        self.cache: ndarray[uint16] | None = None
+        self.stack: ndarray[uint16] | None = None
+        self.address_stack: ndarray[uint16] | None = None
 
         # register memory (registers)
-        self.accumulator: c_uint16 = c_uint16(0)
-        self.program_counter: c_uint16 = c_uint16(0)
-        self.stack_counter: c_uint16 = c_uint16(0)
-        self.address_stack_counter: c_uint16 = c_uint16(0)
+        self.accumulator: uint16 = uint16(0)
+        self.program_counter: uint16 = uint16(0)
+        self.stack_counter: uint16 = uint16(0)
+        self.address_stack_counter: uint16 = uint16(0)
 
     def initialize_memory(self):
         """
         Initializes memory arrays
         """
 
-        self.rom = [c_uint16(0) for _ in range(2**self.ADDRESS_BIT_WIDTH)]
-        self.cache = [c_uint16(0) for _ in range(2**self.ADDRESS_BIT_WIDTH)]
-        self.stack = [c_uint16(0) for _ in range(2**self.ADDRESS_BIT_WIDTH)]
-        self.address_stack = [c_uint16(0) for _ in range(2**self.ADDRESS_BIT_WIDTH)]
+        self.rom = zeros(2**self.ADDRESS_BIT_WIDTH, dtype=uint32)
+        self.cache = zeros(2**self.ADDRESS_BIT_WIDTH, dtype=uint16)
+        self.stack = zeros(2**self.ADDRESS_BIT_WIDTH, dtype=uint16)
+        self.address_stack = zeros(2**self.ADDRESS_BIT_WIDTH, dtype=uint16)
 
     def import_code(self, instructions: list[tuple[int, int, int]]):
         """
@@ -45,14 +45,17 @@ class QTEmulator:
         """
 
         for idx, instruction in enumerate(instructions):
-            memory_flag = c_uint8((instruction[0]) & 1)
-            value = c_uint16(instruction[1])
-            opcode = c_uint8(instruction[2])
+            memory_flag = uint8((instruction[0]) & 1)
+            value = uint16(instruction[1])
+            opcode = uint8(instruction[2])
 
             # M VVVV`VVVV`VVVV`VVVV OOO`OOOO
-            self.rom[idx] = c_uint32((memory_flag.value << 23) + (value.value << 7) + opcode.value)
+            self.rom[idx] = uint32((memory_flag << 23) + (value << 7) + opcode)
 
     def run(self):
         """
         Executes imported code
         """
+
+        while True:
+            flag = self.rom[self.program_counter]
