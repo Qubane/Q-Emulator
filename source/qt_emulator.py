7f9cae7c454239f047ae33a3c431fba7eed7226d
UltraQbik <no1skill@yandex.ru>
2025-01-31 15:18:18 +0300

    Define internal memory

--- source/qt_emulator.py
+++ source/qt_emulator.py
@@ -3,7 +3,36 @@ QT Architecture emulator
 """
 
 
+from ctypes import c_uint16, c_uint32
+
+
 class QTEmulator:
     """
     QT CPU Emulator
     """
+
+    ADDRESS_BIT_WIDTH = 16
+
+    def __init__(self):
+        # program memory
+        self.rom: list[c_uint32] = list()
+
+        # array memory (cache)
+        self.cache: list[c_uint16] = list()
+        self.stack: list[c_uint16] = list()
+        self.address_stack: list[c_uint16] = list()
+
+        # register memory (registers)
+        self.accumulator: c_uint16 = c_uint16(0)
+        self.program_counter: c_uint16 = c_uint16(0)
+        self.stack_counter: c_uint16 = c_uint16(0)
+        self.address_stack_counter: c_uint16 = c_uint16(0)
+
+    def initialize_memory(self):
+        """
+        Initializes memory arrays
+        """
+
+        self.cache = [c_uint16(0) for _ in range(2**self.ADDRESS_BIT_WIDTH)]
+        self.stack = [c_uint16(0) for _ in range(2**self.ADDRESS_BIT_WIDTH)]
+        self.address_stack = [c_uint16(0) for _ in range(2**self.ADDRESS_BIT_WIDTH)]
