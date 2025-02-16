559788947557676ab3a515a5d07855fc6794ce7d
UltraQbik <no1skill@yandex.ru>
2025-02-16 19:10:29 +0300

    Add ScreenModule

--- source/modules.py
+++ source/modules.py
@@ -1,3 +1,14 @@
 """
 Interrupt called modules for emulator
 """
+
+
+class ScreenModule:
+    """
+    Screen module
+    """
+
+    def __init__(self, width: int, height: int, color_depth: int):
+        self.width: int = width
+        self.height: int = height
+        self.color_depth: int = color_depth
