6a4e93ddefaf8665701df7957e5fad59909f962c
UltraQbik <no1skill@yandex.ru>
2025-02-16 20:01:23 +0300

    Make init method

--- source/modules.py
+++ source/modules.py
@@ -3,6 +3,10 @@ Interrupt called modules for emulator
 """
 
 
+import pygame as pg
+from numpy import ndarray, uint16
+
+
 class ScreenModule:
     """
     Screen module
@@ -12,3 +16,14 @@ class ScreenModule:
         self.width: int = width
         self.height: int = height
         self.color_depth: int = color_depth
+
+        self.screen: pg.Surface | None = None
+
+    def init(self):
+        """
+        init screen
+        """
+
+        pg.init()
+        self.screen = pg.display.set_mode((self.width, self.height))
+
